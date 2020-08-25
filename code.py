ae3d0c396df392efb32f19007430536da8108194
derhexenmeister <derhexenmeister@users.noreply.github.com>
2020-08-25 13:39:40 -0700

    Move mode code out of library

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # This file will enable the Piper Command Center to function as a
 # game controller
 #
-from piper_command_center import PiperCommandCenter
-pcc = PiperCommandCenter()
+import piper_command_center_modes
+pcc = piper_command_center_modes.PiperCommandCenter()
 while True:
     pcc.process()
